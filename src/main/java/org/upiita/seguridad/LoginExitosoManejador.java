a219624ac7c98f9274afa9e9ca156267c1737a09
Miguel <izzyboy.m@gmail.com>
2014-07-12 09:42:10 -0500

    Prueba Repo SSH

---
+++ src/main/java/org/upiita/seguridad/LoginExitosoManejador.java
@@ -0,0 +1,5 @@
+package org.upiita.seguridad;
+
+public class LoginExitosoManejador {
+
+}
---
+++ src/main/java/org/upiita/spring/formas/FormaSesion.java
@@ -0,0 +1,22 @@
+package org.upiita.spring.formas;
+
+import java.io.Serializable;
+
+import org.springframework.context.annotation.Scope;
+
+
+
+//@Scope("session")
+public class FormaSesion implements Serializable {
+	
+	private Integer itemsComprados = 0;
+	
+	public Integer getItemsComprados() {
+		return itemsComprados;
+	}
+	
+	public void setItemsComprados(Integer itemsComprados) {
+		this.itemsComprados = itemsComprados;
+	}
+
+}
